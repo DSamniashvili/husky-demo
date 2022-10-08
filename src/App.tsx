da15507df4e9a81bb65854cb175dca8e13af1314
DSamniashvili <deasamniashvili@gmail.com>
2022-10-08 17:38:58 +0400

    test123

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,8 @@ function App() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
-                    Edit <code>src/App.tsx</code> and save to reload. TEST123
+                    Edit <code>src/App.tsx</code> and save to reload.
                 </p>
-                {/* <p>
-                   Thank you!!!
-                </p> */}
                 <a
                     className="App-link"
                     href="https://reactjs.org"
